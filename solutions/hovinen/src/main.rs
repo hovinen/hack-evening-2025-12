bc644060fd408568efb6c1664fdacdc72335e705
Bradford Hovinen <bradford@hovinen.tech>
2025-12-10 19:37:02 +0100

    Refactor to extract grid

--- solutions/hovinen/src/main.rs
+++ solutions/hovinen/src/main.rs
@@ -1,58 +1,88 @@
 use std::{env::args, fs::read_to_string};
 
+#[derive(Copy, Clone)]
+enum Cell {
+    Roll,
+    NoRoll,
+}
+
+impl From<char> for Cell {
+    fn from(value: char) -> Self {
+        if value == '@' {
+            Cell::Roll
+        } else {
+            Cell::NoRoll
+        }
+    }
+}
+
+struct Grid(Vec<Vec<Cell>>);
+
+impl Grid {
+    fn from_str(value: &str) -> Self {
+        let content = value
+            .lines()
+            .filter(|r| !r.trim().is_empty())
+            .map(|r| r.trim().chars().map(Cell::from).collect::<Vec<_>>())
+            .collect::<Vec<Vec<_>>>();
+        Self(content)
+    }
+
+    fn rows(&self) -> usize {
+        self.0.len()
+    }
+
+    fn cols(&self) -> usize {
+        self.0[0].len()
+    }
+
+    fn cell_at(&self, row: usize, col: usize) -> Cell {
+        self.0[row][col]
+    }
+
+    fn neighbours(&self, row: usize, col: usize) -> Vec<Cell> {
+        let mut result = Vec::new();
+        if row > 0 {
+            if col > 0 {
+                result.push(self.0[row - 1][col - 1]);
+            }
+            result.push(self.0[row - 1][col]);
+            if col < self.cols() - 1 {
+                result.push(self.0[row - 1][col + 1]);
+            }
+        }
+        if col > 0 {
+            result.push(self.0[row][col - 1]);
+        }
+        if col < self.cols() - 1 {
+            result.push(self.0[row][col + 1]);
+        }
+        if row < self.rows() - 1 {
+            if col > 0 {
+                result.push(self.0[row + 1][col - 1]);
+            }
+            result.push(self.0[row + 1][col]);
+            if col < self.cols() - 1 {
+                result.push(self.0[row + 1][col + 1]);
+            }
+        }
+        result
+    }
+}
+
 fn count_free_rolls(input: &str) -> usize {
-    let lines = input
-        .lines()
-        .filter(|r| !r.trim().is_empty())
-        .map(|r| r.trim().chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+    let grid = Grid::from_str(input);
     let mut count = 0;
-    for (row, line) in lines.iter().enumerate() {
-        for (col, char) in line.iter().enumerate() {
-            if *char != '@' {
+    for row in 0..grid.rows() {
+        for col in 0..grid.cols() {
+            if !matches!(grid.cell_at(row, col), Cell::Roll) {
                 continue;
             }
-            let mut occupied_sides = 0;
-            if row > 0 {
-                if col > 0 {
-                    if lines[row - 1][col - 1] == '@' {
-                        occupied_sides += 1;
-                    }
-                }
-                if lines[row - 1][col] == '@' {
-                    occupied_sides += 1;
-                }
-                if col < line.len() - 1 {
-                    if lines[row - 1][col + 1] == '@' {
-                        occupied_sides += 1;
-                    }
-                }
-            }
-            if col > 0 {
-                if lines[row][col - 1] == '@' {
-                    occupied_sides += 1;
-                }
-            }
-            if col < line.len() - 1 {
-                if lines[row][col + 1] == '@' {
-                    occupied_sides += 1;
-                }
-            }
-            if row < lines.len() - 1 {
-                if col > 0 {
-                    if lines[row + 1][col - 1] == '@' {
-                        occupied_sides += 1;
-                    }
-                }
-                if lines[row + 1][col] == '@' {
-                    occupied_sides += 1;
-                }
-                if col < line.len() - 1 {
-                    if lines[row + 1][col + 1] == '@' {
-                        occupied_sides += 1;
-                    }
-                }
-            }
+            let occupied_sides = grid
+                .neighbours(row, col)
+                .into_iter()
+                .filter(|n| matches!(n, Cell::Roll))
+                .count();
             if occupied_sides < 4 {
                 count += 1;
             }
